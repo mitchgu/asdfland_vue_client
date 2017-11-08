752935d46b25a1c098f35a3a5f566bed325ebfd7
Mitchell Gu <mitchgu@mit.edu>
2017-11-08 11:19:33 -0500

    Log in/ log out / register functionality

--- src/components/Dash.vue
+++ src/components/Dash.vue
@@ -1,10 +1,27 @@
 <template lang="pug">
-  #dash(:class="{collapsed: collapsed}")
+  #dash(:class="{collapsed: !isRegistered}")
     label#toggle-label(for="toggle", v-text="collapsed? 'Expand' : 'Collapse'")
     input#toggle(v-model="collapsed", type="checkbox", style="display: none")
     #creator-wrap
-      #brand {{ siteName }}
-      #subtitle A revolutionary weight loss program for obese links
+      header#dash-head
+        #brand {{ siteName }}
+        #subtitle A revolutionary weight loss program for obese links
+        #user
+            p 
+              span Welcome, {{ username }}. 
+              span(v-if="!isRegistered")
+                a(href="#" @click.prevent="showLOS('login')") Log in
+                |  or 
+                a(href="#" @click.prevent="showLOS('signup')") sign up
+                |  to manage links 
+              span(v-if="isRegistered")
+                a(href="#" @click.prevent="logout") Logout
+            form#hi(v-if="showLoginOrSignup")
+              input#username(type="text", v-model="LOSUsername", placeholder="username")
+              input#password(type="text", v-model="LOSPassword", placeholder="password")
+              button#los-btn(@click.prevent="submitLOS") {{ loginOrSignup }}
+              a(href="#")
+                i.material-icons(@click="showLoginOrSignup=false") close
       creator
     #listing-wrap
       #listing
@@ -13,16 +30,76 @@
 <script>
 import Creator from './Creator.vue'
 import CONST from '../constants.js'
+import axios from 'axios'
 
 export default {
   name: 'Dash',
   data () {
     return {
       collapsed: true,
-      siteName: CONST.siteName
+      username: 'guest',
+      isRegistered: false,
+      showLoginOrSignup: false,
+      loginOrSignup: 'login',
+      siteName: CONST.siteName,
+      LOSUsername: '',
+      LOSPassword: ''
     }
   },
   methods: {
+    showLOS (which) {
+      this.showLoginOrSignup = true
+      this.loginOrSignup = which
+    },
+    submitLOS () {
+      var reqBody = {
+        Username: this.LOSUsername,
+        Password: this.LOSPassword
+      }
+      if (this.loginOrSignup === 'login') {
+        axios.post('/api/user/login', reqBody).then(response => {
+          if (response.data.success === 'true') {
+            this.username = reqBody.Username
+            this.isRegistered = true
+            this.showLoginOrSignup = false
+            this.LOSUsername = ''
+            this.LOSPassword = ''
+          } else console.log('login fail')
+        }).catch(error => {
+          console.log('login fail ' + error.response.data.msg + '\n')
+        })
+      } else if (this.loginOrSignup === 'signup') {
+        axios.post('/api/user/create', reqBody).then(response => {
+          this.username = reqBody.Username
+          this.isRegistered = true
+          this.showLoginOrSignup = false
+          this.LOSUsername = ''
+          this.LOSPassword = ''
+        }).catch(error => {
+          console.log('login fail ' + error.response.data.msg + '\n')
+        })
+      }
+    },
+    logout () {
+      axios.get('/api/user/logout').then(response => {
+        this.username = 'guest'
+        this.isRegistered = false
+      }).catch(error => {
+        console.log('logout fail ' + error.response.data.msg + '\n')
+      })
+    }
+  },
+  created () {
+    axios.get('/api/session').then(response => {
+      if (response.data.IsRegistered === 'true') {
+        this.username = response.data.Username
+      } else {
+        this.username = 'guest'
+      }
+      this.isRegistered = response.data.IsRegistered === 'true'
+    }).catch(error => {
+      console.log('session info fail ' + error.response.data.msg + '\n')
+    })
   },
   components: {
     Creator
@@ -44,18 +121,25 @@ export default {
 #creator-wrap
   flex: 0
   padding: 0 m-space
+  #dash-head
+    color: chalk-color
+    text-align: center
   #brand
     font-family: chalk-font
-    color: chalk-color
     font-size: brand-font-size
-    text-align: center
     margin-top: l-space
   #subtitle
-    text-align: center
-    margin-bottom: m-space
     color: darken(chalk-color, 5%)
     font-size: s-font-size
-
+  #user
+    margin: m-space 0
+    font-size: s-font-size
+    a, a:visited
+      color: lighten(primary-color, 20%)
+      text-decoration: none
+    a:hover
+      text-decoration: underline
+    
 #listing-wrap
   flex: 1
   #listing
